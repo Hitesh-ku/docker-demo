c97e93cedf9175647d988b904da90480538864cc
Hitesh-ku <hk3696294@gmail.com>
2026-04-01 15:41:33 +0530

    docker commit

---
+++ src/main/java/com/docker_demo/controller/DockerContoller.java
@@ -0,0 +1,12 @@
+package com.docker_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerContoller {
+    @GetMapping("/msg")
+    public String getMessage(){
+        return "Hello World";
+    }
+}
